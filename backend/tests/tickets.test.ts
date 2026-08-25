c6e1c33401cde6b3461053805a9f06b67a79a1e0
Ludvig Möller <Ludvig.Moller@outlook.com>
2026-08-25 14:11:42 +0200

    test: update test to delete the created ticket

--- backend/tests/tickets.test.ts
+++ backend/tests/tickets.test.ts
@@ -1,13 +1,18 @@
 import { describe, it, expect } from "vitest";
 import request from "supertest";
 import app from "../src/app.ts";
+import db from "../src/config/db.ts";
+
+const deleteTicket = db.prepare("DELETE FROM tickets WHERE id = (?)");
 
 describe("POST /api/tickets", () => {
-    it("should create a ticket", async () => {
+    it("should create a ticket and return the id", async () => {
         const res = await request(app)
             .post("/api/tickets");
         
         expect(res.status).toBe(201);
         expect(res.body.id).toBeDefined();
+
+        deleteTicket.run(res.body.id);
     });
 });
