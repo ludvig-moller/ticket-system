48b1474a637ac8769f5e9b8d87dca31639d1fd90
Ludvig Möller <Ludvig.Moller@outlook.com>
2026-08-25 17:15:42 +0200

    style: add createTicket style

--- frontend/src/components/CreateTicket.vue
+++ frontend/src/components/CreateTicket.vue
@@ -1,32 +1,43 @@
+
 <script setup lang="ts">
-import { ref } from 'vue';
-import { createTicket } from '@/services/ticketService';
+    import { ref } from 'vue';
+    import { createTicket } from '@/services/ticketService';
 
-const ticketId = ref("");
-const error = ref("");
+    const ticketId = ref("");
+    const error = ref("");
 
-const click = async () => {
-    const res = await createTicket();
+    const click = async () => {
+        const res = await createTicket();
 
-    if (res != null) {
-        ticketId.value = res;
-        error.value = "";
+        if (res != null) {
+            ticketId.value = res;
+            error.value = "";
+        }
+        else {
+            error.value = "Something went wrong.";
+            ticketId.value = "";
+        }
     }
-    else {
-        error.value = "Something went wrong.";
-        ticketId.value = "";
-    }
-}
 </script>
 
 <template>
-    <button @click="click()">Create a Ticket</button>
+    <div id="createTicket">
+        <button @click="click()">Create a Ticket</button>
 
-    <p v-if="!error"> 
-        {{ ticketId }}
-    </p>
+        <p v-if="!error"> 
+            {{ ticketId }}
+        </p>
 
-    <p v-else class="error">
-        {{ error }}
-    </p>
+        <p v-else class="error">
+            {{ error }}
+        </p>
+    </div>
 </template>
+
+<style lang="css" scoped>
+    #createTicket {
+        display: flex;
+        justify-content: start;
+        gap: 40px;
+    }
+</style>
