e627bf81890d7022c27217d43cc33e5b6990c240
Ludvig Möller <Ludvig.Moller@outlook.com>
2026-08-25 17:15:25 +0200

    style: add basic global style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
+
 <script setup lang="ts">
-import CreateTicket from './components/CreateTicket.vue';
+  import CreateTicket from './components/CreateTicket.vue';
 </script>
 
 <template>
@@ -7,3 +8,7 @@ import CreateTicket from './components/CreateTicket.vue';
     <CreateTicket />
   </main>
 </template>
+
+<style>
+  @import "./global.css";
+</style>
