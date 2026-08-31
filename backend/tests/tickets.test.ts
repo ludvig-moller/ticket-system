ef0ee4a545e62727f2e3fa5673e36ee487564ee6
Ludvig Möller <Ludvig.Moller@outlook.com>
2026-08-31 14:04:36 +0200

    test: add update ticket tests

--- backend/tests/tickets.test.ts
+++ backend/tests/tickets.test.ts
@@ -2,21 +2,16 @@ import { describe, it, expect } from "vitest";
 import request from "supertest";
 import app from "../src/app.ts";
 import db from "../src/config/db.ts";
+import type { Ticket } from "../src/models/Ticket.ts";
 
+const api_key = process.env.API_KEY;
+if (!api_key) {
+    throw Error("API_KEY field in .env file is required");
+}
+
+const getTicket = db.prepare("SELECT * FROM tickets WHERE id = ?")
 const insertTicket = db.prepare("INSERT INTO tickets (id) VALUES (?)");
-const deleteTicket = db.prepare("DELETE FROM tickets WHERE id = (?)");
-
-describe("POST /api/tickets", () => {
-    it("should create a ticket and return the id", async () => {
-        const res = await request(app)
-            .post("/api/tickets");
-        
-        expect(res.status).toBe(201);
-        expect(res.body.id).toBeDefined();
-
-        deleteTicket.run(res.body.id);
-    });
-});
+const deleteTicket = db.prepare("DELETE FROM tickets WHERE id = ?");
 
 describe("GET /api/tickets", () => {
     it("should return unauthorized response when no API key is provided", async () => {
@@ -35,11 +30,6 @@ describe("GET /api/tickets", () => {
     });
 
     it("should return ok when the correct API key is provided", async () => {
-        const api_key = process.env.API_KEY;
-        if (!api_key) {
-            throw Error("API_KEY field in .env file is required");
-        }
-
         const res = await request(app)
             .get("/api/tickets")
             .set("x-api-key", api_key);
@@ -48,11 +38,6 @@ describe("GET /api/tickets", () => {
     });
 
     it("should return all tickets when correct API key is provided", async () => {
-        const api_key = process.env.API_KEY;
-        if (!api_key) {
-            throw Error("API_KEY field in .env file is required");
-        }
-
         const before = await request(app)
             .get("/api/tickets")
             .set("x-api-key", api_key);
@@ -67,6 +52,43 @@ describe("GET /api/tickets", () => {
         expect(after.status).toBe(200);
         expect(after.body).toHaveLength(before.body.length + 1);
         expect(after.body[before.body.length].id).toBe(newTicketId);
+
+        deleteTicket.run(newTicketId);
+    });
+});
+
+describe("POST /api/tickets", () => {
+    it("should create a ticket and return the id", async () => {
+        const res = await request(app)
+            .post("/api/tickets");
+        
+        expect(res.status).toBe(201);
+        expect(res.body.id).toBeDefined();
+
+        deleteTicket.run(res.body.id);
+    });
+});
+
+describe("POST /api/tickets/:id", () => {
+    it("should return 404 when the ticket dosent exist", async () => {
+        const res = await request(app)
+            .post("/api/tickets/invalid-id");
+        
+        expect(res.status).toBe(404);
+    });
+
+    it("should return 204 and update the ticket", async () => {
+        const newTicketId = crypto.randomUUID();
+        insertTicket.run(newTicketId);
+
+        const res = await request(app)
+            .post(`/api/tickets/${newTicketId}`);
+        
+        expect(res.status).toBe(204);
+
+        const ticket = getTicket.get(newTicketId) as Ticket;
+
+        expect(ticket.used).toBe(true);
 
         deleteTicket.run(newTicketId);
     });
